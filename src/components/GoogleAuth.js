fe18d2618472c5331e1b8037c7b04f3ccf4ca51c
Ryo Masutani <ryomasutani@Ryos-MacBook-Pro.local>
2019-09-14 16:00:43 +0900

    Created onSignIn and onSignOut func and added style to login button

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -16,17 +16,40 @@ class GoogleAuth extends Component {
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
           this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
   }
 
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return <div>I don't know if we're singed in</div>;
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>I'm signed in!</div>;
+      return (
+        <button onClick={this.onSignOut} className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
     } else {
-      return <div>You're not signed in</div>;
+      return (
+        <button onClick={this.onSignIn} className="ui red google button">
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
     }
   }
 
