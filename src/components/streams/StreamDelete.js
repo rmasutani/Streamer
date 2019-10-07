5c2bbf5f667d096276941dbcf1ab4f9079f9cc28
Ryo Masutani <ryomasutani@Ryos-MacBook-Pro.local>
2019-10-07 20:57:11 +0900

    Added reusable modal component

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,7 +1,27 @@
 import React from "react";
+import Modal from "../../Modal";
+import history from "../../history";
 
 const StreamDelete = () => {
-  return <div>StreamDelete</div>;
+  const actions = (
+    <React.Fragment>
+      <button className="ui primary button">Delete</button>
+      <button className="ui button">Cancel</button>
+    </React.Fragment>
+  );
+
+  return (
+    <div>
+      StreamDelete
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        //If you want to pass in some actions rather than string, define it above first!
+        actions={actions}
+        onDismiss={() => history.push("/")}
+      />
+    </div>
+  );
 };
 
 export default StreamDelete;
